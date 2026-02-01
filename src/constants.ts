d45eeebaf795a99f98b584d1b6d676db64555607
Pimchanok-ppp <std50146@sriyapai.ac.th>
2026-02-01 20:45:22 +0700

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  primary: '#C6FF00',
+  black: '#0A0A0A',
+  card: '#121212',
+  input: '#1E1E1E',
+};
+
+export const Icons = {
+  Plus: () => "➕",
+  Camera: () => "📸",
+  Upload: () => "📁",
+  Home: () => "🏠",
+  History: () => "📜",
+  Profile: () => "👤"
+};
